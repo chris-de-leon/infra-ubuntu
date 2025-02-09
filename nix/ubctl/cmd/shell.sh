b986e523b021fa6ad057f2e0b42e8e7abc85fa21
chris-de-leon <chrisdeleon333+dev@gmail.com>
2025-02-08 22:07:36 -0800

    fix: error messages for ubctl command and remove unnecessary argument passing to nix command

--- nix/ubctl/cmd/shell.sh
+++ nix/ubctl/cmd/shell.sh
@@ -4,13 +4,13 @@ set -eo pipefail
 
 ASSETS_DIR="${1:-}"
 if [[ -z "${ASSETS_DIR}" ]]; then
-  echo "error: argument 1 (config directory) is required"
+  echo "error: argument 1 (assets directory) is required"
   exit 1
 fi
 
 CONFIG_DIR="${2:-}"
 if [[ -z "${CONFIG_DIR}" ]]; then
-  echo "error: argument 2 (assets directory) is required"
+  echo "error: argument 2 (config directory) is required"
   exit 1
 fi
 
@@ -20,4 +20,4 @@ NIX_DEV_SH="${CONFIG_DIR}/devsh"
   cd "${ASSETS_DIR}" && nix develop --show-trace --no-write-lock-file --profile "${NIX_DEV_SH}" --command bash -c ""
 )
 
-nix develop --show-trace --no-write-lock-file "${NIX_DEV_SH}" "${@:2}"
+nix develop --show-trace --no-write-lock-file "${NIX_DEV_SH}"
