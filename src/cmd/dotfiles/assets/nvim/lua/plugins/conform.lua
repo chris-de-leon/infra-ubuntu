c8ecb5d0ae59d19c0890398e816c09087b5d32b5
chris-de-leon <chrisdeleon333+dev@gmail.com>
2025-02-13 09:30:02 +0000

    chore: remove dotfiles subcommands - these will be merged with the vm subcommands

--- src/cmd/dotfiles/assets/nvim/lua/plugins/conform.lua
+++
@@ -1,9 +0,0 @@
-return {
-  {
-    "stevearc/conform.nvim",
-    -- opts = function(_, opts)
-    --   opts.formatters_by_ft = opts.formatters_by_ft or {}
-    --   opts.formatters_by_ft["typescript"] = { "deno_fmt" }
-    -- end,
-  },
-}
